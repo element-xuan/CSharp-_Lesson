9faca6d3f728b9ac6d2a4180dffe22b984128eae
element <2279938087@qq.com>
2023-10-12 00:35:32 +0800

    1

---
+++ 15.对象的练习/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.对象的练习
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.studentName = "元素";//对象访问字段
+            student1.studentId = 1;
+            student1.studentHeight = 175;
+            student1.studentWeight = 110;
+            student1.introduction();//对象访问方法
+
+            Console.WriteLine("-------------------------------------");
+
+            Student student2 = new Student();
+            student2.studentName = "小强";
+            student2.studentId = 2;
+            student2.studentHeight = 180;
+            student2.studentWeight = 100;
+            student2.introduction();
+        }
+    }
+}
---
+++ 15.对象的练习/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.对象的练习
+{
+    public class Student
+    {
+        //类中可以拥有字段，也可以拥有方法。
+        //如果想要外部能够访问得到 需要公开及public
+
+        #region 类的成员字段
+        /// <summary>
+        /// 学生ID
+        /// </summary>
+        public int studentId;
+        /// <summary>
+        /// 学生姓名
+        /// </summary>
+        public string studentName;
+        /// <summary>
+        /// 学生年龄
+        /// </summary>
+        public int studentAge;
+        /// <summary>
+        /// 学生性别
+        /// </summary>
+        public int studentGender;
+        /// <summary>
+        /// 学生身高
+        /// </summary>
+        public int studentHeight;
+        /// <summary>
+        /// 学生体重
+        /// </summary>
+        public int studentWeight;
+        #endregion
+
+        #region 类中的方法
+        /// <summary>
+        /// 自我介绍
+        /// </summary>
+        public void introduction()
+        {
+            Console.WriteLine($"姓名为{studentName}");
+            Console.WriteLine($"ID为{studentId}");
+            Console.WriteLine($"年龄为{studentAge}");
+            Console.WriteLine($"身高为{studentHeight}");
+            Console.WriteLine($"体重为{studentWeight}");
+        }
+
+        #endregion
+
+        #region 性别枚举后续使用
+        /// <summary>
+        /// 学生性别
+        /// </summary>
+        enum studentGender1
+        {
+            men,
+            woman,
+        }
+        #endregion
+    }
+}
