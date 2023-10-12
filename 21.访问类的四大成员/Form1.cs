272711d9165d3627a22fee99034133171135af9d
element <2279938087@qq.com>
2023-10-12 17:19:29 +0800

    12

---
+++ 21.访问类的四大成员/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21.访问类的四大成员
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student student = new Student();//先创建对象，然后才能访问类中的成员
+            student.StudentId = 1;
+
+            student.StudentName = "Test";
+
+            student.SayHi();
+        }
+    }
+}
---
+++ 21.访问类的四大成员/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21.访问类的四大成员
+{
+    internal class Student
+    {
+        public int StudentId;
+        public string StudentName { get; set; }
+        public void SayHi()
+        {
+            MessageBox.Show("Hi!");
+        }
+    }
+}
